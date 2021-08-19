563618dcb7c54ebbc517b934b6fc19557f9fa979
cihat <cihat02020202@gmail.com>
2021-08-19 20:30:22 +0300

    Add web screen api.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const { app, BrowserWindow } = require("electron")
 const path = require("path")
 
-// Enable live reload for all the files inside your project directory
-require("electron-reload")(__dirname)
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   // eslint-disable-line global-require
@@ -15,6 +12,11 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
   })
 
   // and load the index.html of the app.
@@ -29,10 +31,10 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on("ready", createWindow)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed.
 app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit()
   }
